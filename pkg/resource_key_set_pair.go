56ce80d24b29746b755bbbd1c5e04c8d0236dc7c
Amani215 <amanibrik5@gmail.com>
2023-05-21 17:57:51 +0200

    Added: CRUD implementation

--- pkg/resource_key_set_pair.go
+++ pkg/resource_key_set_pair.go
@@ -2,6 +2,7 @@ package pkg
 
 import (
 	"context"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -35,18 +36,60 @@ func resourceKeySetPair() *schema.Resource {
 	}
 }
 
+func createSet(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*Client).goRedisClient()
+	duration, _ := time.ParseDuration(d.Get("expiry").(string))
+	key := d.Get("key").(string)
+
+	d.SetId(key)
+	client.Expire(ctx, key, duration)
+
+	v := d.Get("value").([]interface{})
+	stringSlice := make([]interface{}, len(v))
+	for i, value := range v {
+		stringSlice[i] = value.(string)
+	}
+	client.SAdd(ctx, key, stringSlice)
+
+	return resourceKeySetPairRead(ctx, d, meta)
+}
+
 func resourceKeySetPairCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	return nil
+	client := meta.(*Client).goRedisClient()
+	if client.Exists(ctx, d.Get("key").(string)).Val() != 0 {
+		return diag.Errorf("Key exists, use update instead")
+	}
+
+	return createSet(ctx, d, meta)
 }
 
 func resourceKeySetPairUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	return nil
+	client := meta.(*Client).goRedisClient()
+	if client.Exists(ctx, d.Get("key").(string)).Val() == 0 {
+		return diag.Errorf("Key does not exist")
+	}
+
+	resourceKeySetPairDelete(ctx, d, meta)
+	return createSet(ctx, d, meta)
 }
 
 func resourceKeySetPairRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*Client).goRedisClient()
+	key := d.Id()
+	val := d.Get("value").([]interface{})
+
+	v, _ := client.SMembers(ctx, key).Result()
+	for i, value := range val {
+		if v[i] != value.(string) {
+			return diag.Errorf("Redis Error")
+		}
+	}
+
 	return nil
 }
 
 func resourceKeySetPairDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*Client).goRedisClient()
+	client.Del(ctx, d.Get("key").(string))
 	return nil
 }
